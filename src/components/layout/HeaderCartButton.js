ca7a2b618d72d72555d54dcf9972d5ad6a18b273
Maria <mariatanshyn@gmail.com>
2021-10-11 19:22:34 +0200

    Added ability to close cart

--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import React from "react";
+import { useContext } from "react";
 import style from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={style.button} onClick={props.onClick}>
       <span className={style.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={style.badge}>3</span>
+      <span className={style.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
