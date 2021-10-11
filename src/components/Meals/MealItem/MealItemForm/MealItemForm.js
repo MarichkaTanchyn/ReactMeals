c36dfadb11a7c12006fc2698561efe666f36533e
Maria <mariatanshyn@gmail.com>
2021-10-11 20:10:37 +0200

    After push on Add, number of meals of Cart is rising

--- src/components/Meals/MealItem/MealItemForm/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm/MealItemForm.js
@@ -1,14 +1,35 @@
-import React from "react";
+import { useRef, useState } from "react";
 import Input from "../../../UI/Input/Input";
 import style from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value; //alwajs string
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().lenght === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+
+  };
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
-          id: "amount" + props.id,
+          id: "amount" ,
           type: "number",
           min: "1",
           max: "5",
@@ -16,6 +37,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
